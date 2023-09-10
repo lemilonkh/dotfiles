a2583e0c7690b59eaa99ce8bd20888a472965345
Milan Gruner <milangruner@gmail.com>
2023-09-10 12:13:56 +0200

    Add more command aliases for W, WA and W
    To prevent errors when holding shift for too long
    
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- lvim/config.lua
+++ lvim/config.lua
@@ -15,6 +15,9 @@ vim.opt.wrap = true           -- wrap lines
 
 vim.api.nvim_create_user_command("Wq", "wq", { nargs = 0 })
 vim.api.nvim_create_user_command("WQ", "wq", { nargs = 0 })
+vim.api.nvim_create_user_command("Wa", "wa", { nargs = 0 })
+vim.api.nvim_create_user_command("WA", "wa", { nargs = 0 })
+vim.api.nvim_create_user_command("W", "w", { nargs = 0 })
 
 -- plugins
 lvim.plugins = {
