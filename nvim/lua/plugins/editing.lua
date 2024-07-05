8f7ca7aeb6e9e0dee2ef5c15972c95e4f9755fd1
Milan Gruner <milangruner@gmail.com>
2024-07-05 12:38:44 +0200

    Install rainbow delimiters nvim plugin

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -1,8 +1,42 @@
 return {
-  "kylechui/nvim-surround",
-  version = "*",
-  event = "VeryLazy",
-  config = function()
-    require("nvim-surround").setup({})
-  end,
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end,
+  },
+  {
+    "hiphish/rainbow-delimiters.nvim",
+    config = function()
+      -- This module contains a number of default definitions
+      local rainbow_delimiters = require("rainbow-delimiters")
+
+      ---@type rainbow_delimiters.config
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [""] = rainbow_delimiters.strategy["global"],
+          vim = rainbow_delimiters.strategy["local"],
+        },
+        query = {
+          [""] = "rainbow-delimiters",
+          lua = "rainbow-blocks",
+        },
+        priority = {
+          [""] = 110,
+          lua = 210,
+        },
+        highlight = {
+          "RainbowDelimiterRed",
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterOrange",
+          "RainbowDelimiterGreen",
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterCyan",
+        },
+      }
+    end,
+  },
 }
